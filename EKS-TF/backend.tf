9c54e00f773cc7f85a39476a65acf72589d86cc6
interndpm <intern@dpminter.com>
2024-07-02 11:34:48 +0700

    Update backend.tf

--- EKS-TF/backend.tf
+++ EKS-TF/backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket         = "my-ews-baket1"
-    region         = "us-east-1"
-    key            = "End-to-End-Kubernetes-DevSecOps-Tetris-Project/EKS-TF/terraform.tfstate"
-    dynamodb_table = "Lock-Files"
+    bucket         = "pp-syd"
+    region         = "ap-southeast-2"
+    key            = "End-to-End-Kubernetes-DevSecOps-Tetris-Project/Jenkins-Server-TF/terraform.tfstate"
+    dynamodb_table = "pp-syd-db2"
     encrypt        = true
   }
   required_version = ">=0.13.0"
